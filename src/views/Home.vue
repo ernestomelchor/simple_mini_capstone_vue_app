1415b333a8425a0ac90896a60e097c75c9ef30b8
ernestomelchor <nurturelove88@gmail.com>
2020-04-06 19:35:03 -0500

    added images using v-bind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,19 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div v-for="product in products">
-      <h2>Name: {{ product.name }}</h2>
-      <h3>Image: {{ product.image_url }}</h3>
+      <h2>Product: {{ product.name }}</h2>
+      <img v-bind:src="product.image_url" alt />
       <h3>Price: {{ product.price }}</h3>
       <p>Description: {{ product.description }}</p>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 300px;
+}
+</style>
 
 <script>
 var axios = require("axios");
