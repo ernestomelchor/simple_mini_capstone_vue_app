bb292ee0bbe5bc5801d4f231839c42e3defadcde
ernestomelchor <nurturelove88@gmail.com>
2020-04-06 19:25:22 -0500

    connected frontend to backend using axios web request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <h2>Name: {{ product.name }}</h2>
+      <h3>Image: {{ product.image_url }}</h3>
+      <h3>Price: {{ product.price }}</h3>
+      <p>Description: {{ product.description }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+var axios = require("axios");
+
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
+      products: []
     };
   },
-  created: function() {},
-  methods: {},
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+    });
+  },
+  methods: {}
 };
 </script>
